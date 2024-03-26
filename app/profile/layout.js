5f684d58e6db5cc5e1bac997592e3a631f9396ea
Elshad Sadigov <sv.elshad@gmail.com>
2024-03-26 21:35:44 +0400

    new commit

--- app/profile/layout.js
+++ app/profile/layout.js
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import backIcon from '../../public/svgs/back-icon.svg'
 import profile1 from '../../public/imgs/profile-1.png'
 import profile2 from '../../public/imgs/profile-2.png'
@@ -16,13 +17,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const layout = ({children}) => {
+  const [pageName, setPageName] = useState('Profil')
+  useEffect(()=>{
+    setPageName(pageName)
+  }, [pageName])
   return (
     <div>
       <div className='flex justify-between items-center px-[80px] py-[23px] border-b-[1px] border-[#D9D9DE]'>
         <div className='flex items-center gap-[5px]'>
             <span className='text-[16px] font-medium text-[#050123]'>Ana səhifə</span>
             <span className='text-[16px] font-medium text-[#050123]'>/</span>
-            <span className='text-[16px] font-medium text-[#050123]'>Profil</span>
+            <span className='text-[16px] font-medium text-[#050123]'>{pageName}</span>
         </div>
         <Link href='/' className='flex items-center gap-[5px]'>
             <Image src={backIcon} alt='back icon' />
@@ -33,7 +38,7 @@ const layout = ({children}) => {
         <div className='bg-[#DCF9FF] rounded-[16px] w-full flex justify-between overflow-hidden h-[240px]'>
             <div className='py-[64px] ps-[80px] flex flex-col w-1/2'>
                 <span className='text-[40px] font-semibold text-[#050123]'>Salam, Aqşin Rəcəbov</span>
-                <span className='mt-[10px] text-[28px] text-[#050123]'>Hesabın idarə edilməsi</span>
+                <span className='mt-[10px] text-[28px] text-[#050123]'>İstifadəçi / Sahibkar</span>
             </div>
             <div className='w-1/2 grid grid-cols-4 gap-[20px] rotate-[-45deg] me-[100px]'>
                 <Image src={profile1} alt='profile-1' className='w-[100%] h-[123px] rounded-[16px]'/>
@@ -53,13 +58,31 @@ const layout = ({children}) => {
       </div>
       <div className='px-[80px] mt-[60px] mb-[130px] flex gap-[32px] items-start'>
         <div className='w-1/4 flex flex-col gap-y-[30px]'>
-            <Link href='/profile' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Hesab Təfərrüatları</Link> 
-            <Link href='/profile/bank-info' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Bank Məlumatları</Link> 
-            <Link href='/profile/change-password' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Şifrə Dəyişikliyi</Link> 
-            <Link href='/profile/active-harrac' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Aktiv Hərraclar</Link> 
-            <Link href='/profile/won-harrac' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Qazanılmış Hərraclar</Link> 
-            <Link href='/profile/activity-harrac' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Hərrac Fəaliyyəti</Link> 
-            <Link href='/' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#EF3340]'>Hesabdan Çıxış</Link> 
+            <Link
+            onClick={()=>setPageName('Qazanılmış Hərraclar')} 
+            href='/profile' 
+            className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Hesab Təfərrüatları</Link> 
+            <Link 
+            onClick={()=>setPageName('Bank Məlumatları')}
+            href='/profile/bank-info' 
+            className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Bank Məlumatları</Link> 
+            <Link 
+            onClick={()=>setPageName('Şifrə Dəyişikliyi')}
+            href='/profile/change-password' 
+            className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Şifrə Dəyişikliyi</Link> 
+            <Link 
+            onClick={()=>setPageName('Aktiv Hərraclar')} 
+            href='/profile/active-harrac' 
+            className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Aktiv Hərraclar</Link> 
+            <Link 
+            onClick={()=>setPageName('Qazanılmış Hərraclar')} 
+            href='/profile/won-harrac' 
+            className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Qazanılmış Hərraclar</Link> 
+            <Link 
+            onClick={()=>setPageName('Hərrac Fəaliyyəti')} 
+            href='/profile/activity-harrac' 
+            className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#050123]'>Hərrac Fəaliyyəti</Link> 
+            <Link  href='/' className='w-full px-[30px] py-[32px] shadow rounded-[16px] text-[20px] font-medium text-[#EF3340]'>Hesabdan Çıxış</Link> 
         </div>
         <div className='w-3/4'>
             {children}
--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
 import logoPoint from '../public/svgs/Logo-point.svg'
 import homeIcon from '../public/svgs/home-icon.svg'
 import searchIcon from '../public/svgs/search-icon.svg'
+import searchIcon2 from '../public/svgs/search-icon2.svg'
 import flagAZ from '../public/svgs/Flag-AZ.svg'
 import downIcon from '../public/svgs/down-icon.svg'
 import like from '../public/svgs/like.svg'
 import Link from 'next/link'
 
 const Header = () => {
+  const [searchShow, setSearchShow] = useState(false)
   return (
-    <div className='bg-[#eefafa] py-[23px] px-[80px] border-b-[1px] border-[#d9d9de] fixed left-0 top-0 w-full mx-auto z-50 flex justify-between items-center'>
+    <div className='bg-[#eefafa] py-[23px] px-[80px] border-b-[1px] border-[#d9d9de] fixed z-[999] left-0 top-0 w-full mx-auto flex justify-between items-center'>
       <Link href='/' className='flex items-start'>
           <span className='font-extrabold text-[30px] text-[#1EA59A]'>HƏR</span>
           <span className='font-extrabold text-[30px] text-[#050123]'>RAC.ORG</span>
@@ -35,9 +38,15 @@ const Header = () => {
           </li>
         </ul>
         <div className='flex items-center gap-[20px]'>
-          <Link href='/'>
-            <Image src={searchIcon} alt='Search icon' />
-          </Link>
+          <div className='cursor-pointer relative'>
+            <Image src={searchIcon} alt='Search icon' onClick={()=>setSearchShow(!searchShow)}/>
+            { searchShow ?
+              <div className='absolute left-[-10px] top-[35px] pe-[5px] py-[5px] ps-[17px] w-[250px] rounded-[8px] bg-[#fff] flex items-center justify-between shadow-md'>
+              <input type="text" placeholder='Axtar...' className='w-[80%] outline-none text-[16px]'/>
+              <Image src={searchIcon2} alt='search icon'/>
+            </div> : null
+            }
+          </div>
           <div className='flex items-center cursor-pointer'>
               <Image src={flagAZ} alt='Flag-AZ' className='rounded-[50%] w-[25px] h-[25px]'/>
               <Image src={downIcon} alt='Down Icon' />
