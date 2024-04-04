d0d69276db3d358958f5b6e5beb693bb6777eab8
Elshad Sadigov <sv.elshad@gmail.com>
2024-04-04 07:42:13 +0400

    header and footer commited

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+        {/* <Header/> */}
         <div className="mt-[90px]">
         {children}
         </div>
-        <Footer/>
+        {/* <Footer/> */}
       </body>
     </html>
   );
